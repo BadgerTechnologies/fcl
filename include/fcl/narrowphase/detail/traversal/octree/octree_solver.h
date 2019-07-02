92b5e9910c444fd338e7fd21ce39344e5167da7d
C. Andy Martin <cam@badger-technologies.com>
2019-07-02 19:44:34 -0400

    Revert "a wip of finding the lower bound and stopping"
    This reverts commit 5a38457cfb83fcd2f102137a4e52b4dfa0851add.

--- include/fcl/narrowphase/detail/traversal/octree/octree_solver.h
+++ include/fcl/narrowphase/detail/traversal/octree/octree_solver.h
@@ -70,8 +70,6 @@ private:
   mutable CollisionResult<S>* cresult;
   mutable DistanceResult<S>* dresult;
 
-  mutable S distance_error_threshold;
-
 public:
   OcTreeSolver(const NarrowPhaseSolver* solver_);
 
@@ -156,13 +154,26 @@ private:
                                    const Transform3<S>& tf1, const Transform3<S>& tf2) const;
 
   template <typename BV>
-  bool OcTreeMeshLowerBoundDistanceRecurse(const OcTree<S>* tree1, const typename OcTree<S>::OcTreeNode* root1, const AABB<S>& bv1,
-                                 const BVHModel<BV>* tree2, int root2,
-                                 const Transform3<S>& tf1, const Transform3<S>& tf2) const;
-  template <typename BV>
   bool OcTreeMeshUpperBoundDistanceRecurse(const OcTree<S>* tree1, const typename OcTree<S>::OcTreeNode* root1, const AABB<S>& bv1,
                                  const BVHModel<BV>* tree2, int root2,
                                  const Transform3<S>& tf1, const Transform3<S>& tf2) const;
+  template <typename BV>
+  struct OcTreeMeshDistanceCandidate
+  {
+    const OcTree<S>* tree1;
+    const typename OcTree<S>::OcTreeNode* leaf1;
+    const BVHModel<BV>* tree2;
+    int leaf2;
+    const typename OcTree<S>::OcTreeNode* leaf1;
+    const Transform3<S>& tf1;
+    const Transform3<S>& tf2;
+  }
+  using OcTreeMeshDistanceCandidateMultiMap = std::multimap<S, OcTreeMeshDistanceCandidate>;
+  template <typename BV>
+  bool OcTreeMeshDistanceFindCandidatesRecurse(const OcTree<S>* tree1, const typename OcTree<S>::OcTreeNode* root1, const AABB<S>& bv1,
+                                 const BVHModel<BV>* tree2, int root2,
+                                 const Transform3<S>& tf1, const Transform3<S>& tf2,
+                                 OcTreeMeshDistanceCandidateMultiMap* candidate_map) const;
   template <typename BV>
   bool OcTreeMeshDistanceRecurse(const OcTree<S>* tree1, const typename OcTree<S>::OcTreeNode* root1, const AABB<S>& bv1,
                                  const BVHModel<BV>* tree2, int root2,
